3e74c9baac76e88162f1f3c89b85a464f6637bf4
Justin <81643826+justinnoel@users.noreply.github.com>
2023-10-10 17:33:45 -0500

    feat: add simple analytics

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -35,6 +35,18 @@ export const Layout: FC<LayoutProps> = ({
 			</head>
 			<body>
 				${children}
+				<!-- 100% privacy-first analytics -->
+				<script
+					async
+					defer
+					src="https://scripts.simpleanalyticscdn.com/latest.js"
+				></script>
+				<noscript
+					><img
+						src="https://queue.simpleanalyticscdn.com/noscript.gif"
+						alt=""
+						referrerpolicy="no-referrer-when-downgrade"
+				/></noscript>
 			</body>
 		</html>`;
 };
